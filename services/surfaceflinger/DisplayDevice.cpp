74c40cf153569701a122138bad82d192acac93c2
Stefan Berger <stefan@octo-bot.(none)>
2016-03-24 00:39:38 +0100

    Revert "DisplayDevice: Backwards compatibility with old EGL"
    This reverts commit e5960bf563567b1edb5f2a0dcf1d36dcc5ecaae5.

--- services/surfaceflinger/DisplayDevice.cpp
+++ services/surfaceflinger/DisplayDevice.cpp
@@ -29,14 +29,6 @@
 
 #include <gui/Surface.h>
 
-#ifdef BOARD_EGL_NEEDS_LEGACY_FB
-#include <ui/FramebufferNativeWindow.h>
-#endif
-
-#include <GLES/gl.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-
 #include <hardware/gralloc.h>
 
 #include "DisplayHardware/DisplaySurface.h"
@@ -86,11 +78,7 @@ DisplayDevice::DisplayDevice(
       mActiveConfig(0)
 {
     mNativeWindow = new Surface(producer, false);
-#ifndef BOARD_EGL_NEEDS_LEGACY_FB
     ANativeWindow* const window = mNativeWindow.get();
-#else
-    ANativeWindow* const window = new FramebufferNativeWindow();
-#endif
 
     /*
      * Create our display's surface
